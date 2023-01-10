2a5401aa6d75224df2a838f0a3fe532c574db9e7
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2023-01-09 20:18:29 -0600

    Initial commit

---
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,17 @@
+package com.in28minutes.junit.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		StringHelper helper = new StringHelper();
+		String actual = helper.truncateAInFirst2Positions("AACD");
+		String expected = "CD";
+		assertEquals(expected, actual);
+	}
+
+}
