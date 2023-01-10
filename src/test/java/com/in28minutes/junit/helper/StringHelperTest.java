d2fe71a54497c86966f5f96a05a8fab696bd30d2
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2023-01-10 12:06:26 -0600

    firstAndLastTest added

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -5,13 +5,27 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringHelperTest {
+	
+	StringHelper helper = new StringHelper();
 
 	@Test
-	public void test() {
-		StringHelper helper = new StringHelper();
-		String actual = helper.truncateAInFirst2Positions("AACD");
-		String expected = "CD";
-		assertEquals(expected, actual);
+	public void testTruncateAInFirst2Positions_AInFirstTwoPositions() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AInFirstPosition() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_False() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_True() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("AB"));
 	}
 
 }
